0d4f463229810d0681370ebb672c3b151b87312d
kaushik134 <kaushikvaghasiya134@gmail.com>
2022-12-31 16:45:30 +0530

    form

--- Express/server.js
+++ Express/server.js
@@ -1,8 +1,14 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
+app.use(express.json())
+app.use(express.urlencoded())
 const PORT = 3030
-app.get("/",(req,res)=>{
-    res.json({msg:"Home"})
+app.get("/",(req,res)=>{   
+    res.sendFile(__dirname+ "/index.html")
+})
+app.get("/login",(req,res)=>{
+    const request = req.body;
+    console.log("Data post ",request)
 })
 app.listen(PORT,()=>{
     console.log(`server ${PORT} connected successfully`)
